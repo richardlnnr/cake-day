52d74a77e95e0df677dbbfbd634aeb614dc6cb6e
Richard Lindner <richard.lindner@fluig.com>
2019-08-13 15:17:29 -0300

    Added sponsor-user code and test

--- projects/cake-day/src/app/sponsor-user/sponsor-user.service.ts
+++ projects/cake-day/src/app/sponsor-user/sponsor-user.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class SponsorUserService {
 
   constructor() { }
+
+  private getDiffDays = (baseD, checkDate) => {  
+    // Garantir que a hora seja sempre zerada
+    baseD.setHours(0, 0, 0, 0);
+    checkDate.setHours(0, 0, 0, 0);
+    
+    const diffTime = Math.abs(checkDate.getTime() - baseD.getTime());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    return diffDays;
+  };
+
+  private getIntervalOrder = (listPeople, diff, intBase) => {
+    return Math.ceil((diff - (Math.floor(diff / (listPeople.length * intBase)) * (listPeople.length * intBase))))
+  };
+
+  getNextPerson = (listPeople, initialDate, checkDate, intervalDays) => {  
+    const diff = this.getDiffDays(initialDate, checkDate);
+    const nextOrder = this.getIntervalOrder(listPeople, diff, intervalDays);
+    const nextPeopleOrder = Math.ceil(nextOrder / intervalDays);
+    
+    return listPeople.find(p => p.order === nextPeopleOrder);
+  };
 }
